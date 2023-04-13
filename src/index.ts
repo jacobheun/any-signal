de5fa8244a3ff12197d0696a694cc1e77cf40f43
Alex Potsides <alex@achingbrain.net>
2023-04-13 16:45:27 +0100

    feat: convert to typescript (#25)
    * feat: convert to typescript
    
    Converts this module to typescript and uses aegir to build and run
    tests.
    
    * chore: fix linting
    
    * fix: return value

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+
+export interface ClearableSignal extends AbortSignal {
+  clear: () => void
+}
+
+/**
+ * Takes an array of AbortSignals and returns a single signal.
+ * If any signals are aborted, the returned signal will be aborted.
+ */
+export function anySignal (signals: Array<AbortSignal | undefined | null>): ClearableSignal {
+  const controller = new globalThis.AbortController()
+
+  function onAbort (): void {
+    controller.abort()
+
+    for (const signal of signals) {
+      if (signal?.removeEventListener != null) {
+        signal.removeEventListener('abort', onAbort)
+      }
+    }
+  }
+
+  for (const signal of signals) {
+    if (signal?.aborted === true) {
+      onAbort()
+      break
+    }
+
+    if (signal?.addEventListener != null) {
+      signal.addEventListener('abort', onAbort)
+    }
+  }
+
+  function clear (): void {
+    for (const signal of signals) {
+      if (signal?.removeEventListener != null) {
+        signal.removeEventListener('abort', onAbort)
+      }
+    }
+  }
+
+  // @ts-expect-error Proxy is not a ClearableSignal
+  return new Proxy(controller.signal, {
+    get (target, p) {
+      if (p === 'clear') {
+        return clear
+      }
+
+      // @ts-expect-error cannot use string to index target type
+      const value = target[p]
+
+      if (typeof value === 'function') {
+        return function (...args: any[]): any {
+          return value.apply(target, args)
+        }
+      }
+
+      return value
+    }
+  })
+}
